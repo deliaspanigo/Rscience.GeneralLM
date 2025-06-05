3ff03f67855a9047df3f12f88bf83d590bba25d3
deliaspanigo <david.elias949@gmail.com>
2025-06-06 00:45:24 +0200

    green buttons

--- R/fn_R_switch_class_from_button_state.R
+++ R/fn_R_switch_class_from_button_state.R
@@ -1,11 +1,14 @@
 #' @export
 fn_R_switch_class_from_button_state <- function(button_state){
   
+# selected_opt <- switch(button_state,
+#        "initial"   = "btn-outline-primary",    # Azul inicial
+#        "confirmed" = "btn-outline-success",    # Verde después de confirmar
+#        "error"     = "btn-outline-danger")
+
 selected_opt <- switch(button_state,
-       "initial"   = "btn-outline-primary",    # Azul inicial
-       "confirmed" = "btn-outline-success",    # Verde después de confirmar
-       "error"     = "btn-outline-danger")
-
-
+                       "initial"   = "btn-primary",    # Azul inicial
+                       "confirmed" = "btn-success",    # Verde después de confirmar
+                       "error"     = "btn-danger")
 return(selected_opt)
 }
